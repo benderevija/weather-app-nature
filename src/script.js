e9845c049787c6041a9de8b2fe6a32c14f488e9d
benderevija <107487131+benderevija@users.noreply.github.com>
2022-08-11 16:15:02 +0200

    Add: JavaScript Properties
    Main Element Live Data

--- src/script.js
+++ src/script.js
@@ -1,1 +1,25 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let weatherDescriptionElement = document.querySelector(
+    "#weather-description"
+  );
+  let feelsLikeElement = document.querySelector("#feels-like");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  weatherDescriptionElement.innerHTML = response.data.weather[0].description;
+  feelsLikeElement.innerHTML = Math.round(response.data.main.feels_like);
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = response.data.wind.speed;
+}
 
+let apiKey = "ad67c6657704952eeec41ea7e3bfce55";
+let units = "metric";
+let city = "New York";
+let apiUrlLink = "https://api.openweathermap.org/data/2.5/weather?";
+let apiUrl = `${apiUrlLink}q=${city}&appid=${apiKey}&units=${units}`;
+
+axios.get(apiUrl).then(displayTemperature);
